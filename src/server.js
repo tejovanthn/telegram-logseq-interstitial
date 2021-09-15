223e57baee5c7081d2d466fd7b9910e3dac24509
Tejovanth N <tj@peppercontent.in>
2021-09-15 09:22:20 +0530

    realised webhooks are async

--- src/server.js
+++ src/server.js
@@ -2,31 +2,36 @@
 import restify from "restify"
 import { updateFile } from "./updateFile.js"
 
+const setupWebhooks = async () => {
+  const connection = await slimbot.getWebhookInfo()
+  if (connection) {
+    await slimbot.deleteWebhook()
+  }
+  await slimbot.setWebhook(`${process.env.HEROKU_WEBHOOK_URL}/bot_updates`)
+}
+
 export default ({ slimbot, api }) => {
   let server = restify.createServer();
   server.use(restify.plugins.bodyParser());
-
-  if (slimbot.getWebhookInfo()) {
-    slimbot.deleteWebhook();
-  }
-  slimbot.setWebhook(`${process.env.HEROKU_WEBHOOK_URL}/bot_updates`)
-  console.log(slimbot.getWebhookInfo());
-
   let recent_id = -1
 
-  server.post('/bot_updates', (req, res) => {
-    let { message } = req.body;
-    console.log(message.date, recent_id, message.message_id)
-    if (message.date > recent_id) {
-      updateFile({ api, message })
-        .then(() => slimbot.sendMessage(message.chat.id, 'Message saved'))
-        .catch(error => { slimbot.sendMessage(message.chat.id, JSON.stringify(error)) });
-      recent_id = message.date
-    }
-    else {
-      console.warn(`retriggered on ${recent_id}`)
-    }
-  });
+  setupWebhooks()
+    .then(() => {
+      server.post('/bot_updates', (req, res) => {
+        let { message } = req.body;
+        console.log(message.date, recent_id, message.message_id)
+        if (message.date > recent_id) {
+          updateFile({ api, message })
+            .then(() => slimbot.sendMessage(message.chat.id, 'Message saved'))
+            .catch(error => { slimbot.sendMessage(message.chat.id, JSON.stringify(error)) });
+          recent_id = message.date
+        }
+        else {
+          console.warn(`retriggered on ${recent_id}`)
+        }
+      });
 
-  server.listen(process.env.PORT || 3000);
+      server.listen(process.env.PORT || 3000);
+    })
+    .catch(console.error)
 }
