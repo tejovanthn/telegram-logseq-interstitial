7f30bd8015e6dc381b0e84341b8b714221ece9fe
Tejovanth N <tj@peppercontent.in>
2021-09-15 08:48:35 +0530

    adds logic to prevent retriggers on webhook

--- src/server.js
+++ src/server.js
@@ -6,14 +6,23 @@ export default ({ slimbot, api }) => {
   let server = restify.createServer();
   server.use(restify.plugins.bodyParser());
 
+  slimbot.deleteWebhook();
   slimbot.setWebhook(`${process.env.HEROKU_WEBHOOK_URL}/bot_updates`)
   console.log(slimbot.getWebhookInfo());
 
+  let recent_id = -1
+
   server.post('/bot_updates', (req, res) => {
     let { message } = req.body;
-    updateFile({ api, message })
-      .then(() => slimbot.sendMessage(message.chat.id, 'Message saved'))
-      .catch(error => { slimbot.sendMessage(message.chat.id, JSON.stringify(error)) });
+    if (message.date > recent_id) {
+      updateFile({ api, message })
+        .then(() => slimbot.sendMessage(message.chat.id, 'Message saved'))
+        .catch(error => { slimbot.sendMessage(message.chat.id, JSON.stringify(error)) });
+      recent_id = message.date
+    }
+    else {
+      console.warn(`retriggered on ${recent_id}`)
+    }
   });
 
   server.listen(process.env.PORT || 3000);
